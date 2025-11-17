33c6895b12a2cd3424c646e49fa7a760d55510ce
ffffffff3rnan <lulabyblueeblackberry@gmail.com>
2025-11-17 15:03:11 +0700

    del

--- frontend/src/pages/OrderSuccessPage.jsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "react-bootstrap";
-export default function OrderSuccessPage() {
-  return (
-    <Container className="text-center mt-5">
-      <h2 className="fw-bold">Pesanan Berhasil!</h2>
-      <p>Terima kasih telah memesan.</p>
-    </Container>
-  );
-}
