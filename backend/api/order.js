82a20e83f443354b16817803f8f383f2678d2a2b
ffffffff3rnan <lulabyblueeblackberry@gmail.com>
2025-11-18 08:04:03 +0700

    fix api

--- backend/api/order.js
+++ backend/api/order.js
@@ -2,7 +2,7 @@ import { ec } from '../edgeConfig';
 
 export default async function handler(req, res) {
   // === CORS ===
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://akatsuki-food-frontend.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
